4a1955ca8a188a1f3168546b23bce77202bd0780
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2022-07-05 20:20:06 +0200

    Update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,13 +1,34 @@
-import { HardhatUserConfig, task } from "hardhat/config";
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+dotenv.config();
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+console.log("process.env.ALCHEMY_URL", process.env.ALCHEMY_URL);
+
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
+  // networks: {
+  //   hardhat: {
+  //     chainId: 1,
+  //     forking: {
+  //       url: process.env.ALCHEMY_URL || "",
+  //     },
+  //     // blockGasLimit: 2100000,
+  //   },
+  // },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
 };
+
 export default config;
