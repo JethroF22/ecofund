db0f5c0a101a02a85af5cf5b85e68f7162e04952
Jethro Fredericks <jethrofredericks.dev@protonmail.com>
2022-07-22 13:02:30 +0200

    Modify hardhat deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const CampaignFactory = await ethers.getContractFactory("CampaignFactory");
+  const campaignFactory = await CampaignFactory.deploy();
 
-  await greeter.deployed();
+  await campaignFactory.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("CampaignFactory deployed to:", campaignFactory.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
